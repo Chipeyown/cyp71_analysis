b5480baa6f2323a41124bdedd34b2f3b46cc2f75
Chipeyown <49014853+Chipeyown@users.noreply.github.com>
2023-05-07 12:26:14 +0800

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,69 +1,40 @@
+#volcano plot
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-df=pd.read_csv('./cyp71.txt',sep='\t',index_col='miRNAid')
-df['cyp71-2/Col0']=df['cyp71-2']/df['Col-0']
-df['cyp71-3/Col0']=df['cyp71-3']/df['Col-0']
-df['Col0']=np.log10(df['Col-0']+1)
-df['cyp71-2']=np.log10(df['cyp71-2']+1)
-df['cyp71-3']=np.log10(df['cyp71-3']+1)
-df=df[['Col0','cyp71-2','cyp71-3','cyp71-2/Col0','cyp71-3/Col0','pvalue_cyp71-2','pvalue_cyp71-3']]
+df=pd.read_csv('./cyp71.txt',sep='\t')
+df['cyp71-2/Col-0']=df['cyp71-2']/df['Col-0']
+df['cyp71-3/Col-0']=df['cyp71-3']/df['Col-0']
 
-texts=['ath-miR159a','ath-miR159b','ath-miR160a','ath-miR165a','ath-miR167a','ath-miR171a','ath-miR172a']
-v=1.5#set value
-
-t=np.arange(0,5,0.2)
-################plot cyp71-2################
-#scatter
-cyp712_up=df[df['cyp71-2/Col0']>=v]
-cyp712_down=df[(df['cyp71-2/Col0']>0) & (df['cyp71-2/Col0']<=1/v)]
-cyp712_down=cyp712_down[cyp712_down['pvalue_cyp71-2']<=0.05]
-cyp712_middle=df[(df['cyp71-2/Col0']<v) & (df['cyp71-2/Col0']>1/v)]
-
-plt.figure(figsize=(6,6),dpi=80)
-labels=['cyp71-2/Col0>=%s'%v,'Col0/cyp71-2>=%s'%v,'Ratio<%s'%v]
-
-up2=plt.scatter(cyp712_up['Col0'],cyp712_up['cyp71-2'],color='darkgrey',marker='.')
-down2=plt.scatter(cyp712_down['Col0'],cyp712_down['cyp71-2'],color='red',marker='.')
-middle2=plt.scatter(cyp712_middle['Col0'],cyp712_middle['cyp71-2'],color='darkgrey',marker='.')
-plt.plot(t,t,'--',color='darkgrey')
-for index in texts:
-    plt.text(df.loc[index][0],df.loc[index][1],index,fontsize=4,horizontalalignment='left')
-
-plt.xlim(0,5)
-plt.ylim(0,5)
-plt.xlabel('log10(Col0 expression+1)')
-plt.ylabel('log10(cyp71-2 expression+1)')
-
-plt.savefig('scatter_cyp71-2.pdf')
-plt.close()
+##############cyp71-2######################
+c2part1=df[(df['pvalue_cyp71-2']<=0.05)&(df['cyp71-2/Col-0']>=3/2)]#upregulated, red dot
+c2part2=df[(df['pvalue_cyp71-2']<=0.05)&(df['cyp71-2/Col-0']<=2/3)]#downregulated, blue dot
+c2part3=df[(df['pvalue_cyp71-2']>0.05)|((df['cyp71-2/Col-0']<3/2)&(df['cyp71-2/Col-0']>2/3))]#grey dot
+plt.axhline(-np.log10(0.05),color='grey',linestyle='--')
+plt.axvline(np.log2(3/2),color='grey',linestyle='--')
+plt.axvline(np.log2(2/3),color='grey',linestyle='--')
+plt.scatter(np.log2(c2part1['cyp71-2/Col-0']),-np.log10(c2part1['pvalue_cyp71-2']),color='r')
+plt.scatter(np.log2(c2part2['cyp71-2/Col-0']),-np.log10(c2part2['pvalue_cyp71-2']),color='b')
+plt.scatter(np.log2(c2part3['cyp71-2/Col-0']),-np.log10(c2part3['pvalue_cyp71-2']),color='grey')
+plt.xlim((-4,4))
+plt.ylim((0,3))
+plt.savefig('volcano_cyp71-2.pdf')
+plt.show()
 
 
-
-################plot cyp71-3################
-#scatter
-cyp713_up=df[df['cyp71-3/Col0']>=v]
-cyp713_down=df[(df['cyp71-3/Col0']>0) & (df['cyp71-3/Col0']<=1/v)]
-cyp713_down=cyp713_down[cyp713_down['pvalue_cyp71-3']<=0.05]
-cyp713_middle=df[(df['cyp71-3/Col0']<v) & (df['cyp71-3/Col0']>1/v)]
-
-plt.figure(figsize=(6,6),dpi=80)
-labels=['cyp71-3/Col0>=%s'%v,'Col0/cyp71-3>=%s'%v,'Ratio<%s'%v]
-
-plt.scatter(cyp713_up['Col0'],cyp713_up['cyp71-3'],color='darkgrey',marker='.')
-plt.scatter(cyp713_down['Col0'],cyp713_down['cyp71-3'],color='red',marker='.')
-plt.scatter(cyp713_middle['Col0'],cyp713_middle['cyp71-3'],color='darkgrey',marker='.')
-plt.plot(t,t,'--',color='darkgrey')
-for index in texts:
-    plt.text(df.loc[index][0],df.loc[index][2],index,fontsize=4,horizontalalignment='left')
-
-plt.xlim(0,5)
-plt.ylim(0,5)
-plt.xlabel('log10(Col0 expression+1)')
-plt.ylabel('log10(cyp71-3 expression+1)')
-
-plt.savefig('scatter_cyp71-3.pdf')
-plt.close()
-
-
+##############cyp71-3######################
+c3part1=df[(df['pvalue_cyp71-3']<=0.05)&(df['cyp71-3/Col-0']>=3/2)]#upregulated, red dot
+c3part2=df[(df['pvalue_cyp71-3']<=0.05)&(df['cyp71-3/Col-0']<=2/3)]#downregulated, blue dot
+c3part3=df[(df['pvalue_cyp71-3']>0.05)|((df['cyp71-3/Col-0']<3/2)&(df['cyp71-3/Col-0']>2/3))]#grey dot
+plt.axhline(-np.log10(0.05),color='grey',linestyle='--')
+plt.axvline(np.log2(3/2),color='grey',linestyle='--')
+plt.axvline(np.log2(2/3),color='grey',linestyle='--')
+plt.scatter(np.log2(c3part1['cyp71-3/Col-0']),-np.log10(c3part1['pvalue_cyp71-3']),color='r')
+plt.scatter(np.log2(c3part2['cyp71-3/Col-0']),-np.log10(c3part2['pvalue_cyp71-3']),color='b')
+plt.scatter(np.log2(c3part3['cyp71-3/Col-0']),-np.log10(c3part3['pvalue_cyp71-3']),color='grey')
+plt.xlim((-4,4))
+plt.ylim((0,3))
+plt.savefig('volcano_cyp71-3.pdf')
+plt.show()
